14abe72717576b319914d52631522dce5f337094
Tadeas Kriz <tadeas.kriz@gmail.com>
2015-03-30 01:26:29 +0200

    Add android-test from main repository.

---
+++ torch-android-test/src/main/java/org/brightify/torch/test/MainTestActivity.java
@@ -0,0 +1,15 @@
+package org.brightify.torch.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author <a href="mailto:tadeas@brightify.org">Tadeas Kriz</a>
+ */
+public class MainTestActivity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
